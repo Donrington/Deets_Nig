efb599252aaf3e9003833f853b3f024d8e06eb2b
Donrington <carryoby@mail.com>
2025-07-02 04:31:29 +0100

    Auto commit: 2025-07-02 04:31:29

--- static/js/footer.js
+++ static/js/footer.js
@@ -1,5 +1,5 @@
 
-const isMobile = window.innerWidth <= 768;
+const isMobile = window.innerWidth <= 480;
 const isTablet = window.innerWidth > 768 && window.innerWidth <= 1024;
 const isDesktop = window.innerWidth > 1024;
 const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
