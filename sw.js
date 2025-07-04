5ca2359b72df8072a52d9617a38daab7d3b79e7b
Donrington <carryoby@mail.com>
2025-07-04 14:59:54 +0100

    Auto commit: 2025-07-04 14:59:53

---
+++ sw.js
@@ -0,0 +1,49 @@
+// Service Worker for DEETS NIG. PWA
+const CACHE_NAME = 'deets-nig-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/script.js',
+  '/favicon-192x192.png',
+  '/favicon-512x512.png',
+  '/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
